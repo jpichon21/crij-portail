3e9cfbad85b3bc0d5af3ce8da1f04cf850fbad91
Kalizo <matthieugodin@live.fr>
2019-04-04 12:25:05 +0200

    Ajout du libellé "Suivez-nous !" pour les RS

--- assets/js/components/Header/Header.js
+++ assets/js/components/Header/Header.js
@@ -15,12 +15,15 @@ export class Header extends React.Component {
             <li><a href={'#'}><i className='far fa-user' />Je me connecte<br />Je m'inscris</a></li>
             <li><a href={'#'}><i className='far fa-envelope' />Inscription<br />Newsletter</a></li>
           </ul>
-          <ul className={'socials'}>
-            <li><a href={'#'}><i style={{ color: '#4267b2' }} className='fab fa-facebook' /></a></li>
-            <li><a href={'#'}><i style={{ color: '#CC0000' }} className='fab fa-youtube' /></a></li>
-            <li><a href={'#'}><i style={{ color: '#00acee' }} className='fab fa-twitter' /></a></li>
-            <li><a href={'#'}><i style={{ color: '#c61689' }} className='fab fa-instagram' /></a></li>
-          </ul>
+          <div className={'socials'}>
+            <span>Suivez-nous !</span>
+            <ul>
+              <li><a href={'#'}><i style={{ color: '#4267b2' }} className='fab fa-facebook' /></a></li>
+              <li><a href={'#'}><i style={{ color: '#CC0000' }} className='fab fa-youtube' /></a></li>
+              <li><a href={'#'}><i style={{ color: '#00acee' }} className='fab fa-twitter' /></a></li>
+              <li><a href={'#'}><i style={{ color: '#c61689' }} className='fab fa-instagram' /></a></li>
+            </ul>
+          </div>
           <a href={'#'}><img className={'logo-ij'} src={Sample.logo_ij} alt={'Information Jeunesse Bourgogne Franche Comte'} /></a>
         </div>
       </header>
