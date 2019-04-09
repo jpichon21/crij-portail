48ce4543c34113568849df0d519dbe2c0889e92f
Kalizo <matthieugodin@live.fr>
2019-04-09 18:04:52 +0200

    Refactoring Graphique + Responsive Category

--- assets/js/Sample.js
+++ assets/js/Sample.js
@@ -47,62 +47,62 @@ export const infoUrl = {
 export const categories = [
   {
     title: 'Catégorie 1',
-    icon: 'build/img/sample-logo-category.png',
-    color: '#ef7f44',
+    icon: 'build/img/sample-icon3.png',
+    color: '#f64747',
     url: 'http://crij.local/#'
   },
   {
     title: 'Catégorie 2',
-    icon: 'build/img/sample-logo-category.png',
-    color: '#ef7f44',
+    icon: 'build/img/sample-icon2.png',
+    color: '#f64747',
     url: 'http://crij.local/#'
   },
   {
     title: 'Catégorie 3',
-    icon: 'build/img/sample-logo-category.png',
-    color: '#ef7f44',
+    icon: 'build/img/sample-icon3.png',
+    color: '#f64747',
     url: 'http://crij.local/#'
   },
   {
     title: 'Catégorie 4',
-    icon: 'build/img/sample-logo-category.png',
-    color: '#ef7f44',
+    icon: 'build/img/sample-icon1.png',
+    color: '#f64747',
     url: 'http://crij.local/#'
   },
   {
     title: 'Catégorie 5',
-    icon: 'build/img/sample-logo-category.png',
-    color: '#ef7f44',
+    icon: 'build/img/sample-icon3.png',
+    color: '#f64747',
     url: 'http://crij.local/#'
   },
   {
     title: 'Site 1',
-    icon: 'build/img/sample-logo-category.png',
-    color: '#457525',
+    icon: 'build/img/sample-icon2.png',
+    color: '#26bcc2',
     url: 'http://crij.local/#'
   },
   {
     title: 'Site 2',
-    icon: 'build/img/sample-logo-category.png',
-    color: '#457525',
+    icon: 'build/img/sample-icon1.png',
+    color: '#26bcc2',
     url: 'http://crij.local/#'
   },
   {
     title: 'Site 3',
-    icon: 'build/img/sample-logo-category.png',
-    color: '#457525',
+    icon: 'build/img/sample-icon3.png',
+    color: '#26bcc2',
     url: 'http://crij.local/#'
   },
   {
     title: 'Site 4',
-    icon: 'build/img/sample-logo-category.png',
-    color: '#457525',
+    icon: 'build/img/sample-icon1.png',
+    color: '#26bcc2',
     url: 'http://crij.local/#'
   },
   {
     title: 'Catégorie 6',
-    icon: 'build/img/sample-logo-category.png',
-    color: '#ef7f44',
+    icon: 'build/img/sample-icon1.png',
+    color: '#f64747',
     url: 'http://crij.local/#'
   }
 ]
@@ -111,19 +111,19 @@ export const news = [
   {
     title: 'Titre 1',
     excerpt: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna...',
-    image: 'build/img/sample-background-news-home.jpg',
+    image: 'build/img/sample-background-news-home1.jpg',
     url: 'http://crij.local/#'
   },
   {
     title: 'Titre 2',
     excerpt: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna...',
-    image: 'build/img/sample-background-news-home.jpg',
+    image: 'build/img/sample-background-news-home2.jpg',
     url: 'http://crij.local/#'
   },
   {
     title: 'Titre 3',
     excerpt: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna...',
-    image: 'build/img/sample-background-news-home.jpg',
+    image: 'build/img/sample-background-news-home3.jpg',
     url: 'http://crij.local/#'
   }
 ]
@@ -131,58 +131,58 @@ export const news = [
 export const sections = [
   {
     title: 'Section 1',
-    icon: 'build/img/sample-logo-section.png',
+    icon: 'build/img/sample-icon4.png',
     thumb: 'build/img/sample-thumb-section.png',
-    color: '#62a438',
+    color: '#26bcc2',
     url: 'http://crij.local/#'
   },
   {
     title: 'Section 2',
-    icon: 'build/img/sample-logo-section.png',
+    icon: 'build/img/sample-icon2.png',
     thumb: 'build/img/sample-thumb-section.png',
-    color: '#3464bd',
+    color: '#ffc831',
     url: 'http://crij.local/#'
   },
   {
     title: 'Section 3',
-    icon: 'build/img/sample-logo-section.png',
+    icon: 'build/img/sample-icon1.png',
     thumb: 'build/img/sample-thumb-section.png',
-    color: '#be4b06',
+    color: '#f64747',
     url: 'http://crij.local/#'
   },
   {
     title: 'Section 4',
-    icon: 'build/img/sample-logo-section.png',
+    icon: 'build/img/sample-icon4.png',
     thumb: 'build/img/sample-thumb-section.png',
-    color: '#62a438',
+    color: '#26bcc2',
     url: 'http://crij.local/#'
   },
   {
     title: 'Section 5',
-    icon: 'build/img/sample-logo-section.png',
+    icon: 'build/img/sample-icon1.png',
     thumb: 'build/img/sample-thumb-section.png',
-    color: '#3464bd',
+    color: '#ffc831',
     url: 'http://crij.local/#'
   },
   {
     title: 'Section 6',
-    icon: 'build/img/sample-logo-section.png',
+    icon: 'build/img/sample-icon2.png',
     thumb: 'build/img/sample-thumb-section.png',
-    color: '#be4b06',
+    color: '#f64747',
     url: 'http://crij.local/#'
   },
   {
     title: 'Section 7',
-    icon: 'build/img/sample-logo-section.png',
+    icon: 'build/img/sample-icon1.png',
     thumb: 'build/img/sample-thumb-section.png',
-    color: '#62a438',
+    color: '#26bcc2',
     url: 'http://crij.local/#'
   },
   {
     title: 'Section 8',
-    icon: 'build/img/sample-logo-section.png',
+    icon: 'build/img/sample-icon2.png',
     thumb: 'build/img/sample-thumb-section.png',
-    color: '#3464bd',
+    color: '#ffc831',
     url: 'http://crij.local/#'
   }
 ]
