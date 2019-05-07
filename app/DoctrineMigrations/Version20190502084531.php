9d21002a8de9c2f4a95bcbb3146a1877f1f4c4a8
Tristan <Webmaster@logomotion.fr>
2019-05-07 11:46:22 +0200

    Correction des fichiers de migrations

--- app/DoctrineMigrations/Version20190502084531.php
+++ app/DoctrineMigrations/Version20190502084531.php
@@ -22,10 +22,10 @@ final class Version20190502084531 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE query DROP FOREIGN KEY FK_24BDB5EBBBDBD894');
-        $this->addSql('DROP INDEX IDX_24BDB5EBBBDBD894 ON query');
-        $this->addSql('ALTER TABLE query DROP contentBlock_id');
-        $this->addSql('ALTER TABLE query_audit DROP contentBlock_id');
+        // $this->addSql('ALTER TABLE query DROP FOREIGN KEY FK_24BDB5EBBBDBD894');
+        // $this->addSql('DROP INDEX IDX_24BDB5EBBBDBD894 ON query');
+        // $this->addSql('ALTER TABLE query DROP contentBlock_id');
+        // $this->addSql('ALTER TABLE query_audit DROP contentBlock_id');
     }
 
     public function down(Schema $schema) : void
@@ -33,9 +33,9 @@ final class Version20190502084531 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE query ADD contentBlock_id INT DEFAULT NULL');
-        $this->addSql('ALTER TABLE query ADD CONSTRAINT FK_24BDB5EBBBDBD894 FOREIGN KEY (contentBlock_id) REFERENCES content_block (id) ON DELETE SET NULL');
-        $this->addSql('CREATE INDEX IDX_24BDB5EBBBDBD894 ON query (contentBlock_id)');
-        $this->addSql('ALTER TABLE query_audit ADD contentBlock_id INT DEFAULT NULL');
+        // $this->addSql('ALTER TABLE query ADD contentBlock_id INT DEFAULT NULL');
+        // $this->addSql('ALTER TABLE query ADD CONSTRAINT FK_24BDB5EBBBDBD894 FOREIGN KEY (contentBlock_id) REFERENCES content_block (id) ON DELETE SET NULL');
+        // $this->addSql('CREATE INDEX IDX_24BDB5EBBBDBD894 ON query (contentBlock_id)');
+        // $this->addSql('ALTER TABLE query_audit ADD contentBlock_id INT DEFAULT NULL');
     }
 }
