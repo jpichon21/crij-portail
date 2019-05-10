fc39441e59f02362fa1b6c5beb884c25536165e2
Tristan <Webmaster@logomotion.fr>
2019-05-10 09:04:56 +0200

    Corrige GET http://localhost/api/contents/1 ne fonctionne pas

--- src/AppBundle/Controller/ContentController.php
+++ src/AppBundle/Controller/ContentController.php
@@ -99,13 +99,15 @@ class ContentController extends Controller
     public function getAction($id)
     {
         $data = $this->repository->find($id);
-        foreach ($data->getContentBlocks() as $contentBlock) {
-            $results = $this->findResults($contentBlock->getQuery());
-            $contentBlock->setResults($results);
-            $contentBlock->setPublicFilters($contentBlock->getQuery()->getPublicFilters());
-        }
         if (!$data) {
             return new JsonResponse(['message' => 'Content not found'], Response::HTTP_NOT_FOUND);
+        }
+        foreach ($data->getContentBlocks() as $contentBlock) {
+            if($contentBlock->getQuery()){
+                $results = $this->findResults($contentBlock->getQuery());
+                $contentBlock->setResults($results);
+                $contentBlock->setPublicFilters($contentBlock->getQuery()->getPublicFilters());
+            }
         }
         return ['data' => $data];
     }
--- src/AppBundle/Entity/ContentBlock.php
+++ src/AppBundle/Entity/ContentBlock.php
@@ -109,7 +109,7 @@ class ContentBlock
      */
     private $results;
 
-        /**
+    /**
      * @var string
      *
      * @Serializer\Expose()
