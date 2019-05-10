48cc4c6cdb608d37996b76b4d1173f10c0e594c1
Tristan <Webmaster@logomotion.fr>
2019-05-10 11:03:41 +0200

    Closes Contextualiser le message d'avertissement de suppression

---
+++ src/AppBundle/Twig/AppBundleExtension.php
@@ -0,0 +1,25 @@
+<?php
+namespace AppBundle\Twig;
+
+use Twig\Extension\AbstractExtension;
+use Twig\TwigTest;
+
+class AppBundleExtension extends AbstractExtension
+{
+    public function getTests()
+    {
+        return [
+            new TwigTest('instanceof', [$this, 'isInstanceof'])
+        ];
+    }
+    
+    /**
+     * @param $object
+     * @param $class
+     * @return bool
+     */
+    public function isInstanceof($object, $class) {
+        $reflectionClass = new \ReflectionClass($class);
+        return $reflectionClass->isInstance($object);
+    }
+}
